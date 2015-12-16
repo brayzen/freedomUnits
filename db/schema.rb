bb0057ef62902711df0d05ebf68ce142593a47bb
Brayzen <brian.ray.biz@gmail.com>
2015-12-15 20:33:22 -0800

    updated rakefile

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20151213235137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "commodities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "days", force: :cascade do |t|
     t.datetime "date"
     t.float    "close"
